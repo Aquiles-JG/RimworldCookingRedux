57366e05032d4a7ce28cf5975897fe2219d1bd30
Taranchuk <nfhfyxer@gmail.com>
2025-07-19 21:15:37 +0300

    update

--- 1.6/Source/CookingReduxMod.cs
+++ 1.6/Source/CookingReduxMod.cs
@@ -6,6 +6,30 @@ using Verse;
 
 namespace CookingRedux
 {
+    public class PlaceWorker_OnOceanWater : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            foreach (IntVec3 c in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            {
+                var terrain = map.terrainGrid.TerrainAt(c);
+                if (terrain != TerrainDefOf.WaterOceanDeep && terrain != TerrainDefOf.WaterOceanShallow)
+                {
+                    return new AcceptanceReport("Aq_NeedsOceanWater".Translate());
+                }
+            }
+
+
+
+            return true;
+        }
+
+
+
+
+
+
+    }
     internal class Thought_Hediff : Thought_Memory
     {
         public bool added = false;
